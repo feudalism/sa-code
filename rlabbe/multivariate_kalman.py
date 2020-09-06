05e1c906a66381341ab317848ac13651b3588925
s <salehah.ar@gmail.com>
2020-09-06 17:28:03 +0200

    update plot in p2

--- rlabbe/multivariate_kalman.py
+++ rlabbe/multivariate_kalman.py
@@ -5,25 +5,25 @@ from cls import Parameters, set_sim_parameters
 
 from cls import KalmanFilter
 from cls import Dog
-from cls import Sensor
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+# Simulation parameters 
 t0 = 0
 tf = 1
 dt = 0.1
 x0 = 0
 
 sim_params = set_sim_parameters(t0, tf, dt, x0)
-sensor = Sensor()
 
 # Initial conditions
 x0 = 10. # [m]
 v0 = 4.5 # [m/s]
 IC = np.array([[x0], [v0]])
 
+# # Define inital variances 
 var_pos = 500. # [m^2] - uncertain about x0
 
 dog_max_speed = 21. # [m/s]
@@ -33,17 +33,14 @@ var_vel = std_vel**2
 cov_pos_vel = 0 # no knowledge
 P = np.diag([var_pos, var_vel])
 
+# Model 
 dog = Dog(x=x0, v=v0, state_cov=P, dt=sim_params.dt)
 filter = KalmanFilter(system=dog, IC=IC)
-  
 
-count = 3
-
-fig, ax = plt.subplots(1,figsize=(6,6))
-plot_dt = 0.1
-
+# Simulation
 print "Dog Tracking"
 x_est = [x0]
+count = 3
 measurements = np.ones(count)
 
 for i, z in enumerate(measurements):    
@@ -61,16 +58,18 @@ for i, z in enumerate(measurements):
     # plt.pause(plot_dt)
     
     # plt.clf()
+    
+# Plotting
+fig, ax = plt.subplots(1,figsize=(6,6))
 
-t_est = range(count)
-t_est.append(count)
+t_est = np.linspace(0, count*dt, count+1)
 ax.plot(t_est, x_est)
 
-t_meas = np.arange(count) + 1.
+t_meas = np.linspace(dt, count*dt, count)
 ax.plot(t_meas, measurements, 'ko')
 
-plt.xlabel('pos in m')
-plt.ylabel('velocity in m/s')
+plt.xlabel('time in s')
+plt.ylabel('position in m')
 
 plt.legend(['$x_{est}$', '$z$'])
 plt.show()
