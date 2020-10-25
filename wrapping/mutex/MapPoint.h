50f7516c9e9ad68d28ee2dd648d14816ffa39e47
Sal <st156899>
2020-10-25 02:18:42 +0200

    [wip] mutex in MapPoint

--- wrapping/mutex/MapPoint.h
+++ wrapping/mutex/MapPoint.h
@@ -40,120 +40,12 @@ namespace ORB_SLAM2
   {
   public:
     MapPoint() = default;
-    MapPoint(const cv::Mat &Pos, KeyFrame *pRefKF, Map *pMap);
-    MapPoint(const cv::Mat &Pos, Map *pMap, Frame *pFrame, const int &idxF);
-
+    MapPoint(const int number);
     virtual ~MapPoint() = default;
-
-    void SetWorldPos(const cv::Mat &Pos);
-    cv::Mat GetWorldPos();
-
-    cv::Mat GetNormal();
-    KeyFrame *GetReferenceKeyFrame();
-
-    std::map<KeyFrame *, size_t> GetObservations();
-    int Observations();
-
-    void AddObservation(KeyFrame *pKF, size_t idx);
-    void EraseObservation(KeyFrame *pKF);
-
-    int GetIndexInKeyFrame(KeyFrame *pKF);
-    bool IsInKeyFrame(KeyFrame *pKF);
-
-    virtual void setBadFlag();
-    bool isBad();
-
-    void Replace(MapPoint *pMP);
-    MapPoint *GetReplaced();
-
-    void IncreaseVisible(int n = 1);
-    void IncreaseFound(int n = 1);
-    float GetFoundRatio();
-    inline int GetFound() { return mnFound; }
-
-    void ComputeDistinctiveDescriptors();
-
-    cv::Mat GetDescriptor();
-
-    void UpdateNormalAndDepth();
-
-    float GetMinDistanceInvariance();
-    float GetMaxDistanceInvariance();
-    int PredictScale(const float &currentDist, KeyFrame *pKF);
-    int PredictScale(const float &currentDist, Frame *pF);
-
-    bool RealPositionKnown;
-    Map *mpMap;
-
-    bool assigned;
-    double covNorm[2 * 2];
-
+    
   public:
-    long unsigned int mnId;
     static long unsigned int nNextId;
-    long int mnFirstKFid;
-    long int mnFirstFrame;
-    int nObs;
-
-    bool lastincorporasion;
-    bool embedded;
-    bool tracked;
-
-    // Variables used by the tracking
-    float mTrackProjX;
-    float mTrackProjY;
-    float mTrackProjXR;
-    bool mbTrackInView;
-    int mnTrackScaleLevel;
-    float mTrackViewCos;
-    long unsigned int mnTrackReferenceForFrame;
-    long unsigned int mnLastFrameSeen;
-
-    // Variables used by local mapping
-    long unsigned int mnBALocalForKF;
-    long unsigned int mnFuseCandidateForKF;
-
-    // Variables used by loop closing
-    long unsigned int mnLoopPointForKF;
-    long unsigned int mnCorrectedByKF;
-    long unsigned int mnCorrectedReference;
-    cv::Mat mPosGBA;
-    long unsigned int mnBAGlobalForKF;
-
-    static std::mutex mGlobalMutex;
-
-  protected:
-    // Position in absolute coordinates
-    cv::Mat mWorldPos;
-    cv::Mat x3D;
-
-    // Keyframes observing the point and associated index in keyframe
-    std::map<KeyFrame *, size_t> mObservations;
-
-    // Mean viewing direction
-    cv::Mat mNormalVector;
-
-    // Best descriptor to fast matching
-    cv::Mat mDescriptor;
-
-    // Reference KeyFrame
-    KeyFrame *mpRefKF;
-
-    // Tracking counters
-    int mnVisible;
-    int mnFound;
-
-    // Bad flag (we do not currently erase MapPoint from memory)
-    bool mbBad;
-    MapPoint *mpReplaced;
-
-    // Scale invariance distances
-    float mfMinDistance;
-    float mfMaxDistance;
-
-    std::mutex mMutexPos;
-    std::mutex mMutexGTPos;
-    std::mutex mMutexFeatures;
+    // static std::mutex mGlobalMutex;
   };
 
 } // namespace ORB_SLAM2
--- wrapping/mutex/swig_config.i
+++ wrapping/mutex/swig_config.i
@@ -3,4 +3,8 @@
 #define SWIG_FILE_WITH_INIT
 #include "MapPoint.h"
 %}
+%feature("immutable", 1) MapPoint::mGlobalMutex;
+%typemap(out, optimal="1") mutex %{
+  $result = SWIG_NewPointerObj(($1_ltype*)&$1, $&1_descriptor, 0);
+%}      
 %include "MapPoint.h"
